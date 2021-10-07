06df073e961370083db96d8c1dffb53a63d56abe
Tom de Geus <tom@geus.me>
2021-10-07 09:34:39 +0200

    Adding "reformat.math"

--- GooseBib/reformat.py
+++ GooseBib/reformat.py
@@ -1,6 +1,21 @@
 import re
 
 
+def _subr(regex, sub, text):
+    """
+    Recursive replacement.
+    """
+
+    # make substitutions, get the number of substitutions "n"
+    text, n = re.subn(regex, sub, text)
+
+    # continue substituting
+    if n:
+        return _subr(regex, sub, text)
+
+    return text
+
+
 def initials(name: str, sep: str = " "):
     """
     Reformat a name such that first names are abbreviated to initials, for example::
@@ -10,6 +25,7 @@ def initials(name: str, sep: str = " "):
 
     :param name: The name formatted as "Lastname, firstname otherfirstname"
     :param sep: Separator to place between initials.
+    :return: Formatted name.
     """
 
     assert len(name.split(",")) > 1
@@ -38,6 +54,31 @@ def initials(name: str, sep: str = " "):
     first = first.strip()
 
     return last + ", " + first.upper()
+
+
+def protect_math(text):
+    """
+    Protect math mode.
+    """
+
+    # skip text without any math
+    if len(text.split(r"{\$}")) < 3:
+        return text
+
+    match = [
+        (re.compile(r"(\{\\\$\})(.*)(\{\\\$\})", re.UNICODE), r"$\2$"),
+        (re.compile(r"(\$\$)(.*)(\$\$)", re.UNICODE), r"$\2$"),
+        (re.compile(r"(\$)(.*)(\{\\{\})(.*)(\$)", re.UNICODE), r"\1\2{\4\5"),
+        (re.compile(r"(\$)(.*)(\{\\}\})(.*)(\$)", re.UNICODE), r"\1\2}\4\5"),
+        (re.compile(r"(\$)(.*)(\{\\_\})(.*)(\$)", re.UNICODE), r"\1\2_\4\5"),
+        (re.compile(r"(\$)(.*)(\{\\^\})(.*)(\$)", re.UNICODE), r"\1\2^\4\5"),
+        (re.compile(r"(\$)(.*)(\\backslash)(.*)(\$)", re.UNICODE), r"\1\2\\\4\5"),
+    ]
+
+    for regex, sub in match:
+        text = _subr(regex, sub, text)
+
+    return text
 
 
 if __name__ == "__main__":
