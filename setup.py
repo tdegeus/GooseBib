f14e8f9de0d75774b47c8576904f8b1f3ff850ba
Tom de Geus <tom@geus.me>
2022-04-22 15:20:55 +0200

    Improving setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
 
+project_name = "texplain"
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name="GooseBib",
+    name=project_name,
     license="MIT",
     author="Tom de Geus",
     author_email="tom@geus.me",
     description="Reformat BibTeX files",
-    long_description="Reformat BibTeX files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords="LaTeX; BibTeX",
-    url="https://github.com/tdegeus/GooseBib",
+    url=f"https://github.com/tdegeus/{project_name:s}",
     packages=find_packages(),
     package_data={"": ["*.yaml"]},
-    use_scm_version={"write_to": "GooseBib/_version.py"},
+    use_scm_version={"write_to": f"{project_name}/_version.py"},
     setup_requires=["setuptools_scm"],
     install_requires=[
         "arxiv",
@@ -27,16 +35,16 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "GbibCheckAuthors = GooseBib.cli.GbibCheckAuthors:main",
-            "GbibCheckKeys = GooseBib.cli.GbibCheckKeys:main",
-            "GbibCheckLink = GooseBib.cli.GbibCheckLink:main",
-            "GbibClean = GooseBib.bibtex:GbibClean",
-            "GbibList = GooseBib.cli.GbibList:main",
-            "GbibParse = GooseBib.cli.GbibParse:main",
-            "GbibSelect = GooseBib.cli.GbibSelect:main",
-            "GbibSelectAlias = GooseBib.cli.GbibSelectAlias:main",
-            "GbibShowAuthorRename = GooseBib.bibtex:GbibShowAuthorRename",
-            "GbibDiscover = GooseBib.bibtex:GbibDiscover",
+            f"GbibCheckAuthors = {project_name}.cli.GbibCheckAuthors:main",
+            f"GbibCheckKeys = {project_name}.cli.GbibCheckKeys:main",
+            f"GbibCheckLink = {project_name}.cli.GbibCheckLink:main",
+            f"GbibClean = {project_name}.bibtex:GbibClean",
+            f"GbibList = {project_name}.cli.GbibList:main",
+            f"GbibParse = {project_name}.cli.GbibParse:main",
+            f"GbibSelect = {project_name}.cli.GbibSelect:main",
+            f"GbibSelectAlias = {project_name}.cli.GbibSelectAlias:main",
+            f"GbibShowAuthorRename = {project_name}.bibtex:GbibShowAuthorRename",
+            f"GbibDiscover = {project_name}.bibtex:GbibDiscover",
         ]
     },
 )
